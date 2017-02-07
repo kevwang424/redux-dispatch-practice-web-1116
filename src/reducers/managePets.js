4ef8813fe4ed01e72dd1aaef19793dfd3bc60793
Kevin Wang <kevinwang@Kevins-MacBook-Air.local>
2017-02-07 09:14:48 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,46 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={pets: []}, action){
+  switch (action.type) {
+    case "ADD_PET":
+      return Object.assign({}, state, {pets: [...state.pets, action.payload]})
+    case "REMOVE_PET":
+      return Object.assign({}, state, {pets: state.pets.filter(function(pet){
+      if(pet.id !== action.payload){
+        return pet
+      }
+    })})
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state,action)
+  render()
 }
 
 export function render(){
 
+  let pets = state.pets.map(function(pet){
+    return `<li>${pet.name}</li>`
+  })
+
+  let petsJoin = pets.join(" ")
+
+  let container = document.getElementById('container')
+
+  container.innerHtml= `<ul>${petsJoin}</ul>`
+
+
 }
+
+// dispatch({type: "@@INIT"})
+
+// state.pets.forEach(function(pet){
+//   let node = document.createElement("li")
+//   let textnode = document.createTextNode(pet.name)
+//   node.appendChild(textnode)
+//   list.appendChild(node)
+// })
